563e49a312e218e096287f8d39f787e14f4473ef
Shawn <121857051@qq.com>
2018-07-25 15:42:57 +0800

    update : refine 290

--- easy/290wordPattern.py
+++ easy/290wordPattern.py
@@ -15,3 +15,71 @@ class Solution:
             return True
         else:
             return False
+
+
+class Solution:
+    def wordPattern(self, pattern, str):
+        """
+        :type pattern: str
+        :type str: str
+        :rtype: bool
+        """
+        a=str.split(' ')
+        b=pattern
+        if len(b)!=len(a):
+            return False
+        c=[]
+        dic={}
+        for i in range(len(b)):
+            if b[i] in dic:
+                c.append(dic[b[i]])
+            else:
+                dic[b[i]]=a[i]
+                c.append(dic[b[i]])
+        if len(set(dic.values()))!=len(set(b)):
+            return False
+        return a==c
+
+
+class Solution:
+    def wordPattern(self, pattern, str):
+        """
+        :type pattern: str
+        :type str: str
+        :rtype: bool
+        """
+        a=str.split(' ')
+        b=pattern
+        if len(b)!=len(a):
+            return False
+        dic={}
+        for i in range(len(b)):
+            if b[i] in dic:
+                if dic[b[i]]!=a[i]:
+                    return False
+            else:
+                dic[b[i]]=a[i]
+        if len(set(dic.values()))!=len(set(b)):
+            return False
+        return True
+
+
+class Solution:
+    def wordPattern(self, pattern, str):
+        """
+        :type pattern: str
+        :type str: str
+        :rtype: bool
+        """
+        a=str.split(' ')
+        z=zip(a,pattern)
+        return (len(set(a))==len(set(pattern))==len(set(z)) and len(a)==len(pattern))
+
+class Solution:
+    def wordPattern(self, pattern, str):
+        """
+        :type pattern: str
+        :type str: str
+        :rtype: bool
+        """
+        return (len(set(str.split()))==len(set(pattern))==len(set(zip(str.split(),pattern))) and len(str.split())==len(pattern))
