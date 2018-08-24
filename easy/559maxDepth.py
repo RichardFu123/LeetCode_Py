122d3a1a49078a1dc0e4df3f7e92563a079243ae
Shawn <121857051@qq.com>
2018-08-24 15:05:52 +0800

    update : add code 559

---
+++ easy/559maxDepth.py
@@ -0,0 +1,14 @@
+"""
+# Definition for a Node.
+class Node(object):
+    def __init__(self, val, children):
+        self.val = val
+        self.children = children
+"""
+class Solution(object):
+    def maxDepth(self, root):
+        """
+        :type root: Node
+        :rtype: int
+        """
+        return 0 if not root else max(self.maxDepth(i) for i in root.children+[None])+1
